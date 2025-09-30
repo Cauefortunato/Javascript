ee8dec27162269417762f03d0435f8b8ae5f3d92
Cauê Fortunato <cauefortunato09@gmail.com>
2025-09-30 10:37:06 -0300

    aula 30/09/2025

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -3,5 +3,6 @@
 import http from 'http'; // importa o módulo HTTP
 http.createServer((req,res) => { // cria um servidor
     res.writeHead(200, {'Content-Type': 'text/plain'}); // cabeçalho da resposta
-    res.end('ABSDNKLAFYDHIASOF '); // corpo da resposta
+    res.end('OLA MUNDO'); // corpo da resposta
 }).listen(1236); // o servidor escuta a porta 1236
+console.log('Servidor rodando em http://localhost:1236/'); // mensagem no console
